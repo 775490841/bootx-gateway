f999ba068b5424161d2ba6b2f3c3735aaefb1618
xial@plian.net <a12345678>
2020-05-08 13:51:11 +0800

    网关项目

---
+++ src/main/java/com/boox/gateway/BootxGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.boox.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootxGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootxGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boox/gateway/BootxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boox.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootxGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
