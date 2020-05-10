b114a7cf336b63980a35b68975dfd0a0a23b46b3
1169794338@qq.com <Liziyi521521>
2020-05-10 12:21:54 +0800

    init

--- src/main/java/com/boox/gateway/config/CorsConfig.java
+++ src/main/java/com/boox/gateway/config/CorsConfig.java
@@ -1,27 +1,52 @@
 package com.boox.gateway.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
-
-import javax.annotation.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
-@ConditionalOnBean(GlobalCorsProperties.class)
 public class CorsConfig {
 
-    @Resource
-    private  GlobalCorsProperties globalCorsProperties;
+  @Bean
+  public WebFilter corsFilter() {
+    return (ServerWebExchange ctx, WebFilterChain chain) -> {
+      ServerHttpRequest request = ctx.getRequest();
+      if (CorsUtils.isCorsRequest(request)) {
+        HttpHeaders requestHeaders = request.getHeaders();
+        ServerHttpResponse response = ctx.getResponse();
+        HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
+        HttpHeaders headers = response.getHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestHeaders.getOrigin());
+        headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, requestHeaders.getAccessControlRequestHeaders());
+        if(requestMethod != null){
+          headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
+        }
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+        headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "18000L");
+        if (request.getMethod() == HttpMethod.OPTIONS) {
+          response.setStatusCode(HttpStatus.OK);
+          return Mono.empty();
+        }
 
-    @Bean
-    public CorsWebFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-        globalCorsProperties.getCorsConfigurations().forEach((path,corsConfiguration)->source.registerCorsConfiguration(path, corsConfiguration));
-        return new CorsWebFilter(source);
-    }
+      }
+      return chain.filter(ctx);
+    };
+  }
 
+  @Bean
+  public ServerCodecConfigurer serverCodecConfigurer() {
+    return new DefaultServerCodecConfigurer();
+  }
 }
